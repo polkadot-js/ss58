3dfb2a594bd2837df4eb7a8e2035b24bde8bec24
Gav <gavin@parity.io>
2018-08-07 17:21:43 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,40 @@
+import {blake2b} from 'blakejs'
+import bs58 from 'bs58'
+
+export function ss58_decode(address) {
+	let a
+	try {
+		a = bs58.decode(address)
+	}
+	catch (e) {
+		return null
+	}
+	if (a[0] == 42) {
+		if (a.length == 32 + 1 + 2) {
+			let address = a.slice(0, 33)
+			let hash = blake2b(address)
+			if (a[33] == hash[0] && a[34] == hash[1]) {
+				return address.slice(1)
+			} else {
+				// invalid checksum
+				return null
+			}
+		} else {
+			// Invalid length.
+			return null
+		}
+	} else {
+		// Invalid version.
+		return null
+	}
+}
+
+export function ss58_encode(address) {
+	if (address.length != 32) {
+		return null
+	}
+	let bytes = new Uint8Array([42, ...address])
+	let hash = blake2b(bytes)
+	let complete = new Uint8Array([...bytes, hash[0], hash[1]])
+	return bs58.encode(complete)
+}
